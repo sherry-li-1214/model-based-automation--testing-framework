6ee7d2ba7b18b3cb930b5606d1082e03fb37523a
sherry-li-1214 <19400030+sherry-li-1214@users.noreply.github.com>
2025-12-11 13:02:02 +1100

    Update token API URL and clear request body fields

--- k6PerformanceTesting/utils/getToken.js
+++ k6PerformanceTesting/utils/getToken.js
@@ -10,27 +10,27 @@ import http from 'k6/http';
 
 export function getFakeTokenstaffJWTFromForge(ocv_id) {
     // Token API URL  
-    let url = `https://joker.identity-services-sit-fr.apps.x.gcpnp../ig-int/jwt`;
+    let url = `https://joker.identity-services/ig-int/jwt`;
     // Request body
     const requestBody=JSON.stringify({
       "cts": "OAUTH2_STATELESS_GRANT",
-      "auditTrackingId": "17f492ac-b3b6-43b2-a0bd-078223fe5e2c-613999",
+      "auditTrackingId": "",
       "subname": "salesforce",
-      "iss": "https://identity-services-sit-int-gw.apps-int.x.gcpnp../am/oauth2/system",
+      "iss": "https://identity-services-sit/am/oauth2/system",
       "tokenName": "access_token",
       "token_type": "Bearer",
       "aud": "salesforce",
       "grant_type": "client_credentials",
       "realm": "/system",
-      "jti": "GslMKFIk_FptcHd6Q2fJq7iF-is",
+      "jti": "",
       "role": "",
       "rqf": {
-        "sub": "f5e54c6b-f363-4d83-a366-a1826625c445",
+        "sub": "",
         "persona": {
-          "_id": "c691cddc-49a8-4abb-9fd3-fda0ab9e4784",
+          "_id": "",
           "type": "retail"
         },
-        "ocvid": "1000552160"
+        "ocvid": ""
       },
       "acr": "IAL4.AAL2.FAL2",
       "act": {
@@ -39,11 +39,9 @@ export function getFakeTokenstaffJWTFromForge(ocv_id) {
       "amr": [
         "pwd"
       ],
-      "._token_type": "staffRqfCustomer",
+      "._token_type": "",
       "scope": [
-        "au.(BBB).crm",
-        "aegis-introspect",
-        "au.(BBB).fraud"
+       
       ],
       "expires_in": 31536000
     });
