d238323c9c9cfb53e088479dd640925d5e1c0f46
sherry-li-1214 <19400030+sherry-li-1214@users.noreply.github.com>
2025-12-11 13:02:36 +1100

    Fix TLS auth file paths in tlsAuth.js
    Updated file paths for TLS certificate and key.

--- k6PerformanceTesting/utils/tlsAuth.js
+++ k6PerformanceTesting/utils/tlsAuth.js
@@ -3,8 +3,8 @@ function getTLSAuth () {
        
         let  tlsAuth= [
             {
-              cert: open('/Users/lix25/Documents/api ssl cert/sli-nonprod-postman-(AAA)-nonprod...com 2.pem'),
-              key: open('/Users/lix25/Documents/api ssl cert/private.key.pem'),
+              cert: open('/Users/com 2.pem'),
+              key: open('/Users//private.key.pem'),
             },
           ]
             return tlsAuth
